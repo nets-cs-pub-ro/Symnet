a77cae99806eea379f8d86a49e69cea5b1985886
Radu Stoenescu <radu.stoe@gmail.com>
2014-08-05 21:41:20 +0300

    Add union and normalize interval ops.

--- src/main/scala/org/change/symbolicexec/package.scala
+++ src/main/scala/org/change/symbolicexec/package.scala
@@ -3,5 +3,32 @@ package org.change
 package object symbolicexec {
 
   type Interval = (Long, Long)
+  type ValueSet = List[Interval]
+
+  def doIntersect(a: Interval, b: Interval) =
+    (a._1 <= b._1 && b._1 <= a._2) || (a._1 <= b._1 && b._2 <= a._2)
+
+  def intersection(a: Interval, b: Interval) =
+    if (a._1 <= b._1 && b._1 <= a._2) Some((b._1, Math.min(b._2, a._2)))
+    else if (a._1 <= b._2 && b._1 <= a._2) Some((Math.max(b._1, a._1), b._2))
+    else None
+
+  def unionOfIntersecting(a: Interval, b: Interval) =
+    if (doIntersect(a,b)) Some((Math.min(a._1, b._1), Math.max(a._2, b._2)))
+    else None
+
+  def normalize(set: List[Interval]): List[Interval] = {
+    normalizeSorted(set.sortBy(_._1))
+  }
+
+  def normalizeSorted(s: ValueSet): ValueSet = s match {
+    case i1 :: i2 :: rest => unionOfIntersecting(i1, i2) match {
+      case Some(union) => normalizeSorted(union :: rest)
+      case None => i1 :: normalizeSorted(i2 :: rest)
+    }
+    case _ => s
+  }
+
+  def union(all: List[ValueSet]): ValueSet = normalize(all.flatten)
 
 }
