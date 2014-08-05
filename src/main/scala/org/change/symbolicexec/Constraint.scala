eb64825f40c25d20ac0747f3420f80917c4b3f5c
Radu Stoenescu <radu.stoe@gmail.com>
2014-08-05 19:51:00 +0300

    Constraint to set

--- src/main/scala/org/change/symbolicexec/Constraint.scala
+++ src/main/scala/org/change/symbolicexec/Constraint.scala
@@ -1,11 +1,45 @@
 package org.change.symbolicexec
 
-trait Constraint
+trait Constraint {
+  /**
+   * Materialize a constraint as the set of possible values.
+   * @param valueType For bound checks.
+   */
+  def asSet(valueType: NumericType = NumericType()): List[Interval] = Nil
+}
 
-case class LT(v: Long) extends Constraint
-case class LTE(v: Long) extends Constraint
-case class GT(v: Long) extends Constraint
-case class GTE(v: Long) extends Constraint
-case class E(v: Long) extends Constraint
-case class Range(v1: Long, v2: Long) extends Constraint
-case class RangeSeries(ranges: List[Range]) extends Constraint
+case class LT(v: Long) extends Constraint {
+  override def asSet(valueType: NumericType): List[(Long, Long)] = {
+    List((valueType.min, Math.min(v-1, valueType.max)))
+  }
+}
+case class LTE(v: Long) extends Constraint {
+  override def asSet(valueType: NumericType): List[(Long, Long)] = {
+    List((valueType.min, Math.min(v, valueType.max)))
+  }
+}
+case class GT(v: Long) extends Constraint {
+  override def asSet(valueType: NumericType): List[(Long, Long)] = {
+    List((Math.max(v+1, valueType.min), valueType.max))
+  }
+}
+case class GTE(v: Long) extends Constraint{
+  override def asSet(valueType: NumericType): List[(Long, Long)] = {
+    List((Math.max(v, valueType.min), valueType.max))
+  }
+}
+case class E(v: Long) extends Constraint{
+  override def asSet(valueType: NumericType): List[(Long, Long)] =
+    if (valueType.min <= v && v <= valueType.max) List((v,v))
+    else Nil
+}
+case class Range(v1: Long, v2: Long) extends Constraint {
+  override def asSet(valueType: NumericType): List[(Long, Long)] = {
+    List((Math.max(v1, valueType.min), Math.min(v2, valueType.max)))
+  }
+}
+case class RangeSeries(ranges: List[Range]) extends Constraint {
+  override def asSet(valueType: NumericType): List[(Long, Long)] =
+// Warning: The result should be normalized
+    ranges.map(_.asSet(valueType)).flatten
+}
--- src/main/scala/org/change/symbolicexec/NumericType.scala
+++ src/main/scala/org/change/symbolicexec/NumericType.scala
@@ -1,8 +1,14 @@
 package org.change.symbolicexec
 
-trait NumericType extends Type {
-
+class NumericType extends Type {
+  override def name = "GenericNumeric"
   def min: Long = 0
   def max: Long = Long.MaxValue
   def admissibleRange: Interval = (min, max)
+}
+
+object NumericType {
+  private lazy val instance = new NumericType
+
+  def apply() = instance
 }
