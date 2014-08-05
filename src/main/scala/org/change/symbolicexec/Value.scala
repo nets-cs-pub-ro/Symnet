50020c4d17c722aa4fb80b510b51f16920414b67
Radu Stoenescu <radu.stoe@gmail.com>
2014-08-05 19:31:19 +0300

    Doc of value methods.

--- src/main/scala/org/change/symbolicexec/Value.scala
+++ src/main/scala/org/change/symbolicexec/Value.scala
@@ -2,8 +2,21 @@ package org.change.symbolicexec
 
 case class Value(valueType: NumericType, constraints: List[Constraint], evalCache: List[Interval] = Nil) {
 
+  /**
+   * Returns the set of possible concrete values given the constraints, from cache.
+   * These may not be valid anymore.
+   */
   def eval: List[Interval] = evalCache
+
+  /**
+   * Determines the set of possible concrete values given the constraints.
+   * The results are computed then cached.
+   */
   def forceEval: List[Interval] = evalCache
+
+  /**
+   * Are there any possible values ?
+   */
   def valid: Boolean = ! eval.isEmpty
 
 }
