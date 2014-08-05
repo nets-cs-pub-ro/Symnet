081a1a907084ea684b27dc0970fb1492609f38eb
Radu Stoenescu <radu.stoe@gmail.com>
2014-08-06 02:27:17 +0300

    Fix intersection and finish intersect.

--- src/main/scala/org/change/symbolicexec/package.scala
+++ src/main/scala/org/change/symbolicexec/package.scala
@@ -10,7 +10,9 @@ package object symbolicexec {
 
   def intersection(a: Interval, b: Interval) =
     if (a._1 <= b._1 && b._1 <= a._2) Some((b._1, Math.min(b._2, a._2)))
-    else if (a._1 <= b._2 && b._1 <= a._2) Some((Math.max(b._1, a._1), b._2))
+    else if (a._1 <= b._2 && b._2 <= a._2) Some((Math.max(b._1, a._1), b._2))
+    else if (a._1 <= b._1 && a._2 >= b._2) Some(b._1, b._2)
+    else if (a._1 >= b._1 && a._2 <= b._2) Some(a._1, a._2)
     else None
 
   def unionOfIntersecting(a: Interval, b: Interval) =
@@ -30,6 +32,7 @@ package object symbolicexec {
   }
 
   def union(all: List[ValueSet]): ValueSet = normalize(all.flatten)
+
   def complement(s: ValueSet, t: NumericType = NumericType()): ValueSet = s match {
     case Nil => List((t.min, t.max))
     case List((a, b)) if a > t.min && b < t.max => normalize(List((t.min, a-1), (b+1, t.max)))
@@ -45,5 +48,25 @@ package object symbolicexec {
         (if (s.last._2 < t.max) List((s.last._2+1, t.max)) else Nil) ++
         aux)
     }
+  }
+
+  def intersect(all: List[ValueSet]): ValueSet = {
+    def looper(a: ValueSet, b: ValueSet): ValueSet = a match {
+      case Nil => Nil
+      case i :: rest => {
+        val stripNeedless = b.dropWhile(_._2 < i._1)
+        stripNeedless
+          .takeWhile(i._2 > _._1)
+          .map(that =>
+            intersection(i, that))
+          .filter(_ match {
+            case Some(_) => true
+            case _ => false})
+          .map(_ match {
+            case Some(r) => r}) ++ looper(rest, stripNeedless)
+      }
+    }
+    val nAll = all.map(normalize)
+    nAll.tail.foldLeft(nAll.head)(looper)
   }
 }
