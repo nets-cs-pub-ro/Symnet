6266723cf013d25a7fe309a62b17104e90e815cd
Radu Stoenescu <radu.stoe@gmail.com>
2014-08-06 18:02:12 +0300

    Add constrain method on value.

--- src/main/scala/org/change/symbolicexec/Value.scala
+++ src/main/scala/org/change/symbolicexec/Value.scala
@@ -1,18 +1,21 @@
 package org.change.symbolicexec
 
-/**
- * Warning, eval Cache should be made Option since Nil is a valid value.
- * @param constraints
- * @param valueType
- * @param evalCache
- */
-case class Value(constraints: List[Constraint], valueType: NumericType = NumericType(), var evalCache: List[Interval] = Nil) {
+case class Value(constraints: List[Constraint], valueType: NumericType = NumericType(), var evalCache: Option[List[Interval]] = None) {
+
+//  If no cache to start with, compute it
+  evalCache match {
+    case None => evalCache = Some(constraints.foldLeft(valueType.admissibleSet)((s, c) => applyConstraint(s,c,valueType)))
+    case Some(_) =>
+  }
 
   /**
    * Returns the set of possible concrete values given the constraints, from cache.
    * These may not be valid anymore.
    */
-  def eval: List[Interval] = evalCache
+  def eval: List[Interval] = evalCache match {
+    case None => forceEval
+    case Some(vals) => vals
+  }
 
   /**
    * Determines the set of possible concrete values given the constraints.
@@ -22,14 +25,21 @@ case class Value(constraints: List[Constraint], valueType: NumericType = Numeric
     /**
      * Compute and store
      */
-    evalCache = constraints.foldLeft(valueType.admissibleSet)( (s, c) => applyConstraint(s,c,valueType))
-    evalCache
+    val vs = constraints.foldLeft(valueType.admissibleSet)((s, c) => applyConstraint(s,c,valueType))
+    evalCache = Some(vs)
+    vs
   }
 
   /**
    * Are there any possible values ?
    */
   def valid: Boolean = ! eval.isEmpty
+
+  def constrain(c: Constraint): Value =
+    Value(c :: constraints, valueType, Some(applyConstraint(eval, c, valueType)))
+
+  def constrain(cs: List[Constraint]): Value =
+    Value(cs ++ constraints, valueType, Some(cs.foldLeft(eval)((cache, c) => applyConstraint(cache, c, valueType))))
 
 }
 
@@ -40,5 +50,7 @@ object Value {
    * @param valueType
    * @return
    */
-  def apply(c: Constraint, valueType: NumericType = NumericType()): Value = Value(List(c), valueType)
+  def fromConstraint(c: Constraint, valueType: NumericType = NumericType()): Value = Value(List(c), valueType)
+
+  def unconstrained(valueType: NumericType = NumericType()): Value = Value(Nil, valueType)
 }
