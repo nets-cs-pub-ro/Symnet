6b15caa51cf06882d909e8b18941b04440db6222
Radu Stoenescu <radu.stoe@gmail.com>
2015-03-30 12:52:26 +0300

    [+] Some more juice for MemSpace api.

--- src/main/scala/org/change/v2/analysis/memory/MemorySpace.scala
+++ src/main/scala/org/change/v2/analysis/memory/MemorySpace.scala
@@ -11,15 +11,27 @@ import scala.collection.mutable.{Map => MutableMap}
 */
 class MemorySpace(val symbolSpace: MutableMap[String, MemorySymbol] = MutableMap()) {
 
+  /**
+   * Introsection PUBLIC API
+   */
+
+  def version: Int = age
+  def symbolSSAVersion(id: String): Int = symbolSpace.get(id).map(_.valueStack.length).getOrElse(0)
+  def symbolIsDefined = { symbolSSAVersion(_) > 0 }
+  def symbolIsDefinedAndVisible(id: String) = { symbolIsDefined(id) && ! symbolSpace(id).hidden }
+
   private var age = -1
 
   /**
    * The memory is initially void, so there has to be a way to bring a symbol to existence.
    *
+   * The new symbol is initially hidden because there is no value defined in the SSA stack.
+   * Moreover, this is why it uses update instead of mutate.
+   *
    * COMEBACK: Would be nice to access previous args in the expression for default new ones.
    */
   private def createSymbol(symbolId: String, symbolType: Option[NumericType] = None): MemorySpace =
-    selfMutate { m => m.symbolSpace.get(symbolId) match {
+    selfUpdate { m => m.symbolSpace.get(symbolId) match {
         case None => m.symbolSpace += ((symbolId, new MemorySymbol(
           symbolType.getOrElse(TypeUtils.canonicalForSymbol(symbolId)))))
         case _ => // NoOp, a symbol with the same id exists, nothing to do.
