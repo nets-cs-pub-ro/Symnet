7d1284192bb724b727e9627341aa7d206cd9f076
Radu Stoenescu <radu.stoe@gmail.com>
2015-04-01 11:11:48 +0300

    [+] REMOVE op.

--- src/main/scala/org/change/v2/analysis/memory/MemorySpace.scala
+++ src/main/scala/org/change/v2/analysis/memory/MemorySpace.scala
@@ -16,9 +16,19 @@ class MemorySpace(val symbolSpace: MutableMap[String, MemorySymbol] = MutableMap
    */
 
   def version: Int = age
+
   def symbolSSAVersion(id: String): Int = symbolSpace.get(id).map(_.valueStack.length).getOrElse(0)
-  def symbolIsDefined = { symbolSSAVersion(_) > 0 }
+
+  def symbolIsDefined(id: String) = { symbolSSAVersion(id) > 0 }
   def symbolIsDefinedAndVisible(id: String) = { symbolIsDefined(id) && ! symbolSpace(id).hidden }
+
+  /**
+   * Operational PUBLIC API
+   */
+  def REMOVE = remove _
+
+
+//  def rewrite
 
   private var age = -1
 
@@ -27,6 +37,9 @@ class MemorySpace(val symbolSpace: MutableMap[String, MemorySymbol] = MutableMap
    *
    * The new symbol is initially hidden because there is no value defined in the SSA stack.
    * Moreover, this is why it uses update instead of mutate.
+   *
+   * This is method performs the trick of presenting an infinite memory backed by an on-demand
+   * allocation system.
    *
    * COMEBACK: Would be nice to access previous args in the expression for default new ones.
    */
@@ -37,6 +50,16 @@ class MemorySpace(val symbolSpace: MutableMap[String, MemorySymbol] = MutableMap
         case _ => // NoOp, a symbol with the same id exists, nothing to do.
       }
     }
+
+  /**
+   * If symbol is defined and visible, it is hidden, otherwise, NoOp
+   * @param id
+   * @return
+   */
+  private def remove(id: String) = if (symbolSpace.contains(id) && ! symbolSpace(id).hidden)
+    selfMutate(_.symbolSpace(id).hide)
+  else
+    this
 
   private def selfUpdate(f: (MemorySpace => Unit)): MemorySpace = {
     mutateAndReturn(this)(f)
