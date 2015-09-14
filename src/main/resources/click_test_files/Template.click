a85fdd217f9bc72a05ecb7866c0c0e14778b9caf
Costin <Costin@Raiciu.com>
2015-09-14 13:04:44 +0300

    added tcp fields including flags, preparing for options processing

--- src/main/resources/click_test_files/Template.click
+++ src/main/resources/click_test_files/Template.click
@@ -1,4 +1,4 @@
 src :: FromDevice
 dst :: ToDevice
 
-src -> IPsecESPEncap(15) -> IPsecAES(1,blabla) -> IPsecAES(0,blabla1) -> IPsecESPDecap() -> dst
+src -> IPsecESPEncap(15) -> IPsecAES(1,blabla) -> IPsecAES(0,blabla) -> IPsecESPDecap() -> dst
--- src/main/scala/org/change/v2/analysis/processingmodels/State.scala
+++ src/main/scala/org/change/v2/analysis/processingmodels/State.scala
@@ -4,7 +4,8 @@ import org.change.v2.analysis.memory.MemorySpace
 
 import org.change.v2.util.canonicalnames._
 import org.change.v2.analysis.processingmodels.instructions._
-import org.change.v2.analysis.expression.concrete.SymbolicValue
+import org.change.v2.analysis.expression.concrete._
+import org.change.v2.analysis.expression.concrete.nonprimitive._
 import org.change.v2.analysis.memory.TagExp._
 import org.change.v2.analysis.memory.Tag
 
@@ -79,8 +80,35 @@ object State {
      Allocate(TcpAck, 32),
      Assign(TcpAck, SymbolicValue()),
 
-     CreateTag("END", L4Tag + 12000)
+     Allocate(TcpDataOffset, 4),
+     Assign(TcpDataOffset, ConstantValue(160)),
 
+     Allocate(TcpReserved,3),
+     Assign(TcpReserved,SymbolicValue()),
+
+     Allocate(TcpFlagNS,1),
+     Assign(TcpFlagNS,ConstantValue(0)),
+     Allocate(TcpFlagCWR,1),
+     Assign(TcpFlagCWR,ConstantValue(0)),
+     Allocate(TcpFlagECE,1),
+     Assign(TcpFlagECE,ConstantValue(0)),
+     Allocate(TcpFlagURG,1),
+     Assign(TcpFlagURG,ConstantValue(0)),
+     Allocate(TcpFlagACK,1),
+     Assign(TcpFlagACK,SymbolicValue()),
+     Allocate(TcpFlagACK,1),
+     Assign(TcpFlagACK,SymbolicValue()),
+     Allocate(TcpFlagSYN,1),
+     Assign(TcpFlagSYN,SymbolicValue()),
+     Allocate(TcpFlagRST,1),
+     Assign(TcpFlagRST,ConstantValue(0)),
+     Allocate(TcpFlagPSH,1),
+     Assign(TcpFlagPSH,ConstantValue(0)),
+
+     //CreateTag("PAYLOAD", :+:(L4Tag,:@(TcpDataOffset)),
+     //Allocate(Tag("PAYLOAD"),12000),
+     //Assign(Tag("PAYLOAD"),SymbolicValue()),
+     CreateTag("END", L4Tag + 12000)
    )(bigBang, true)
 
    afterBigBang._1.head
