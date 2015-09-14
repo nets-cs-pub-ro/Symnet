a41770ad395b34022e2c92c1aa48b45b7b44d294
Radu Stoenescu <radu.stoe@gmail.com>
2015-09-14 12:25:44 +0300

    [+] Add sample Cisco switch, make it parseable and add support for it in the IPClassifier code.

---
+++ src/test/scala/clickfiletoexecutor/CiscoBehemotSwitch.scala
@@ -0,0 +1,18 @@
+package clickfiletoexecutor
+
+import org.change.parser.clickfile.ClickToAbstractNetwork
+import org.change.v2.executor.clickabstractnetwork.ClickExecutionContext
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Author: Radu Stoenescu
+ * Don't be a stranger,  symnetic.7.radustoe@spamgourmet.com
+ */
+class CiscoBehemotSwitch extends FlatSpec with Matchers {
+
+  "The behemot" should "be parseable to a click executor" in {
+    val absNet = ClickToAbstractNetwork.buildConfig("src/main/resources/click_test_files/CiscoEtherSwitch.click")
+    val executor = ClickExecutionContext(absNet)
+  }
+
+}
