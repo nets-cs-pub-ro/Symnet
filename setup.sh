2be5cabf6388a3c5132c62a4df67152b36f44485
Radu Stoenescu <radu.stoe@gmail.com>
2015-10-07 12:45:34 +0300

    [+] Some additions.

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@
 #
 # If a different version is preferred, the ScalaZ3 jar must be recomplied
 # against this jdk. You're on shaky ground there.
-sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk 
+sudo apt-get install -y openjdk-8-jdk 
 
 # We use sbt for building Symnet.
 #
@@ -16,10 +17,10 @@ wget https://dl.bintray.com/sbt/native-packages/sbt/0.13.9/sbt-0.13.9.tgz
 tar xf sbt-0.13.9.tgz 
 rm *.tgz
 # Make sure sbt is in path.
-sudo ln -s sbt/bin/sbt /usr/local/bin/sbt
+sudo ln -s ~/sbt/bin/sbt /usr/local/bin/sbt
 
 # Install git and grab Symnet
-sudo apt-get install git-core 
+sudo apt-get install -y git-core 
 git clone https://github.com/radustoenescu/Symnetic.git
 
 # Build it and test the rig.
