82a8a3e82c561f43e36d2341354f4775f2ce9b3f
Radu Stoenescu <radu.stoe@gmail.com>
2016-08-16 16:01:43 +0300

    + also print the states for sefl executor

--- src/main/scala/org/change/v2/runners/sefl/SEFLExecutor.scala
+++ src/main/scala/org/change/v2/runners/sefl/SEFLExecutor.scala
@@ -1,5 +1,7 @@
 package org.change.v2.runners.sefl
 
+import java.io.{File, FileOutputStream, PrintStream}
+
 import org.change.v2.analysis.memory.State
 import org.change.v2.analysis.processingmodels.Instruction
 import org.change.v2.executor.clickabstractnetwork.ClickExecutionContext
@@ -25,6 +27,18 @@ object SEFLExecutor {
 
     println("Possible: " + states._1.length)
     println("Impossible: " + states._2.length)
+
+    val (successful, failed) = (states._1, states._2)
+    val output = new PrintStream(new FileOutputStream(new File("sefl.output")))
+
+    output.println(
+      successful.map(_.jsonString).mkString("Successful: {\n", "\n", "}\n") +
+        failed.map(_.jsonString).mkString("Failed: {\n", "\n", "}\n")
+    )
+
+    output.close()
+
+    println("Check output @ sefl.output")
   }
 
 }
