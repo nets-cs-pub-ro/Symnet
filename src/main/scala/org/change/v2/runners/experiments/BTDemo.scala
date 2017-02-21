eaef8aa2cba0ecf52196cab22437ddf261542423
Radu Stoenescu <radu.stoe@gmail.com>
2017-02-21 10:12:53 +0200

    + bt demo brief run instructions

--- src/main/scala/org/change/v2/runners/experiments/BTDemo.scala
+++ src/main/scala/org/change/v2/runners/experiments/BTDemo.scala
@@ -9,20 +9,28 @@ import org.change.v2.executor.clickabstractnetwork.executionlogging.JsonLogger
 import org.change.v2.executor.clickabstractnetwork.AggregatedBuilder._
 
 /**
-  * Created by radu on 15.01.2017.
+  * This is an example of how to analyze a heterogeneous data plane.
+  *
+  * It can parse and analyze routers, switches and arbitrary Click files.
+  *
+  * The path to the folder that includes the data plane specification is the first parameter of the main
+  * command line parameter. If one is not provided, then it defaults to "src/main/resources/21cn"
   */
 object BTDemo {
 
   def main(args: Array[String]): Unit = {
-    val exe = executorFromFolder(new File("src/main/resources/21cn"), Map(
+
+    val dataPlaneFolder = if (args.length > 2) args(1) else "src/main/resources/21cn"
+
+    val exe = executorFromFolder(new File(dataPlaneFolder), Map(
       "switch" -> OptimizedSwitch.trivialSwitchNetworkConfig _,
       "click" -> {f => ClickToAbstractNetwork.buildConfig(f, prefixedElements = true)},
       "router" -> OptimizedRouter.trivialRouterNetwrokConfig _
     )).setLogger(JsonLogger)
 
-//    val start = System.currentTimeMillis()
+    val start = System.currentTimeMillis()
         exe.untilDone(true)
-//    println(System.currentTimeMillis() - start)
+    println("It took me:" + (System.currentTimeMillis() - start))
   }
 
 }
