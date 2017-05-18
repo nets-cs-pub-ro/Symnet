7949f8633bb10d70b0135d888b668c15c5994992
Calin Cruceru <calin@rosedu.org>
2017-05-19 02:04:38 +0300

    Rename classes to disambiguate

--- src/main/scala/org/change/v2/analysis/expression/concrete/nonprimitive/And.scala
+++ src/main/scala/org/change/v2/analysis/expression/concrete/nonprimitive/And.scala
@@ -5,24 +5,24 @@ import org.change.v2.analysis.memory.{State, Value}
 import org.change.v2.analysis.z3.Z3Util
 import z3.scala.{Z3Solver, Z3AST}
 
-case class And(a: Value, b: Value) extends Expression {
+case class BitwiseAnd(a: Value, b: Value) extends Expression {
 
   override def toZ3(solver: Option[Z3Solver] = None): (Z3AST, Option[Z3Solver]) = {
     val (aAst, aSolver) = a.toZ3(solver)
     val (bAst, bSolver) = b.toZ3(aSolver)
 
-    (Z3Util.z3Context.mkAnd(aAst, bAst), bSolver)
+    (Z3Util.z3Context.mkBVAnd(aAst, bAst), bSolver)
   }
 
   override def toString = s"(${a.e} & ${b.e})"
 }
 
-case class :&:(left: FloatingExpression, right: FloatingExpression) extends FloatingExpression {
+case class :&(left: FloatingExpression, right: FloatingExpression) extends FloatingExpression {
 
   override def instantiate(s: State): Either[Expression, String] = {
     (left instantiate s) match {
       case Left(e1) => (right instantiate s) match {
-        case Left(e2) => Left(And(Value(e1), Value(e2)))
+        case Left(e2) => Left(BitwiseAnd(Value(e1), Value(e2)))
         case cause2@Right(_) => cause2
       }
       case cause1@Right(_) => cause1
